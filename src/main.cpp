188823818223cc940a438663a63b576f8d4317e8
Andreas Wegner <andreaswegner@posteo.de>
2022-06-12 15:53:14 +0200

    Added value clamping to sun rotation via user input

--- src/main.cpp
+++ src/main.cpp
@@ -155,9 +155,15 @@ void charCallback(unsigned int keycode)
     {
     case 'u': planet->position.y += 0.2f; break;
     case 'i': planet->position.y -= 0.2f; break;
-    case 'q': sphere->rotation.z += 1.0f; break;
-    case 'w': sphere->rotation.z -= 1.0f; break;
     case 'g': planetStopped = !planetStopped; break;
+    case 'q':
+        sphere->rotation.z += 1.0f;
+        sphere->rotation.z = glm::clamp(sphere->rotation.z, 0.0f, 360.0f);
+        break;
+    case 'w':
+        sphere->rotation.z -= 1.0f;
+        sphere->rotation.z = glm::clamp(sphere->rotation.z, 0.0f, 360.0f);
+        break;
     }
 }
 
